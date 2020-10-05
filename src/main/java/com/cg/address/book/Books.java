f1babea748faa16ffdbb0711d9de321ebe954169
nkneeraj <nkneerj641@gmail.com>
2020-10-05 17:42:21 +0530

    [NK]Added UC6: For multiple address books with unique name.

---
+++ src/main/java/com/cg/address/book/Books.java
@@ -0,0 +1,71 @@
+package com.cg.address.book;
+
+import java.util.*;
+
+public class Books {
+	private Scanner sc = new Scanner(System.in);
+
+	Map<String, Book> books = new HashMap<>();
+
+	public Books() {
+		books = new HashMap<>();
+	}
+
+	public void openBook() {
+		System.out.println("Enter name of addressbook");
+		String bookName = sc.nextLine();
+		boolean bookPresent = books.containsKey(bookName);
+
+		if (bookPresent) {
+			Book book = books.get(bookName);
+			System.out.println("Welcome to Book: "+bookName);
+			System.out.println("Enter 1 to add contact");
+			System.out.println("Enter 2 to edit contact");
+			System.out.println("Enter 3 to delete existing contact");
+			System.out.println("Enter 4 to exit");
+
+			int input = sc.nextInt();
+			sc.nextLine();
+			while (input != 4) {
+				if (input == 1) {
+					book.addContact();
+				} else if (input == 2) {
+					System.out.println("Enter First Name :");
+					String firstName = sc.nextLine();
+					book.updateContact(firstName);
+				} else if (input == 3) {
+					System.out.println("Enter First Name of contact that need to be deleted.");
+					String firstName = sc.nextLine();
+					book.deleteContact(firstName);
+				} else if (input == 4) {
+					System.out.println("Exit");
+					break;
+				} else
+					System.out.println("Wrong entry");
+				System.out.println("Enter 1 to add contact");
+				System.out.println("Enter 2 to edit contact");
+				System.out.println("Enter 3 to delete an existing contact");
+				System.out.println("Enter 4 to exit");
+				input = sc.nextInt();
+				sc.nextLine();
+			}
+		}
+		else
+			System.out.println("Book not found");
+	}
+
+	public void createBook() {
+		
+		Book book = new Book();
+		System.out.println("Enter name of new book: ");
+		String bookName = sc.nextLine();
+		boolean bookPresent = books.containsKey(bookName);
+		while(bookPresent)
+			{System.out.println("Book already present");
+			 bookName = sc.nextLine();
+			 bookPresent = books.containsKey(bookName);
+			}
+		books.put(bookName,book);
+	}
+
+}
--- src/main/java/com/cg/addressbook/Main.java
+++ src/main/java/com/cg/addressbook/Main.java
@@ -12,37 +12,36 @@ public class Main {
 
 		Scanner sc = new Scanner(System.in);
 		Book book = new Book();
+		Books books = new Books();
 		ContactServiceImp contactService = new ContactServiceImp();
 		Contact contact = new Contact();
-		System.out.println("Welcome to Book");
-		System.out.println("Enter 1 to add contact");
-		System.out.println("Enter 2 to edit contact");
-		System.out.println("Enter 3 to delete existing contact");
-		System.out.println("Enter 4 to exit");
-
+		System.out.println("Welcome to AddressBooks");
+		System.out.println("Enter 1 to Open existing addressbook");
+		System.out.println("Enter 2 to Create new AddressBook");
+//		System.out.println("Enter 3 to delete existing contact");
+		System.out.println("Enter 3 to exit");
+//
 		int input = sc.nextInt();
 		sc.nextLine();
-		while (input != 4) {
+		while (input != 3) {
 			if (input == 1) {
-				book.addContact();
-			} else if (input == 2) {
-				System.out.println("Enter First Name :");
-				String firstName = sc.nextLine();
-				book.updateContact(firstName);
-			} else if(input==3) {
-				System.out.println("Enter First Name of contact that need to be deleted.");
-				String firstName = sc.nextLine();
-				book.deleteContact(firstName);
-			}	else if (input ==4) {
+				books.openBook();
+			} else if (input == 2) {	
+				books.createBook();
+//			} else if(input==3) {
+//				System.out.println("Enter First Name of contact that need to be deleted.");
+//				String firstName = sc.nextLine();
+//				book.deleteContact(firstName);
+			}	else if (input ==3) {
 				System.out.println("Exit");
 				break;
 			}
 			else
 			System.out.println("Wrong entry");
-			System.out.println("Enter 1 to add contact");
-			System.out.println("Enter 2 to edit contact");
-			System.out.println("Enter 3 to delete an existing contact");
-			System.out.println("Enter 4 to exit");
+			System.out.println("Enter 1 to open existing AddressBook");
+			System.out.println("Enter 2 to create new AddressBook");
+//			System.out.println("Enter 3 to delete an existing contact");
+			System.out.println("Enter 3 to exit");
 			input = sc.nextInt();
 			sc.nextLine();
 		}
