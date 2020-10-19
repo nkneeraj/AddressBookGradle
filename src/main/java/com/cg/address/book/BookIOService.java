6412347c2d754aca9846a937b268e159d1870156
nkneeraj <nkneerj641@gmail.com>
2020-10-20 00:13:23 +0530

    [NK]Added UC14:JSON read and write added.

--- src/main/java/com/cg/address/book/BookIOService.java
+++ src/main/java/com/cg/address/book/BookIOService.java
@@ -1,18 +1,27 @@
 package com.cg.address.book;
 
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.Reader;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
 import com.cg.addressbook.contact.Contact;
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
 
 public class BookIOService {
 
 	public static String READ_FILE = "./readFile.txt";
 	public static String WRITE_FILE = "writeFile.txt";
+	
+	public static final String SAMPLE_JSON_FILE_PATH = "./file1.json";
+	public static final String SAMPLE_JSON_FILE_PATH2 = "./file.json";
 
 	public void writeData(List<Contact> contactList) {
 		StringBuffer empBuffer = new StringBuffer();
@@ -57,5 +66,36 @@ public class BookIOService {
 			e.printStackTrace();
 		}
 		return entries;
+	}
+	
+	public boolean writeJsonData(List<Contact> contactList) {
+		Gson gson = new Gson();
+		String json = gson.toJson(contactList);
+		try {
+			FileWriter fileWriter = new FileWriter(SAMPLE_JSON_FILE_PATH);
+			fileWriter.write(json);
+			fileWriter.close();
+			return true;
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return false;
+		}
+	}
+	
+	public boolean readJsonFile(){
+		try {
+			Reader reader = Files.newBufferedReader(Paths.get(SAMPLE_JSON_FILE_PATH2));
+			JsonParser jsonParser = new JsonParser();
+			JsonElement obj = jsonParser.parse(reader);
+			JsonArray contactList = (JsonArray) obj;
+			System.out.println(contactList);
+			
+			return true;
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return false;
+		}
 	}
 }
